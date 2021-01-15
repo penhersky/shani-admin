ba5889a7f6e9faa126abd3e432ff8f00156e16c7
penhersky <penxerscky@gmail.com>
2021-01-15 16:34:33 +0200

    add chart

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -6,22 +6,53 @@ import Info from './info';
 import Settings from './settings';
 import Graph from './Graph';
 
+import Chart from './chart';
+
 import './style.css';
 
-const dashboard = () => (
-  <div>
-    <Welcome />
-    <Card>
-      <CardContent className='d-content'>
-        <div className='top'>
-          <Graph />
-          <Settings />
-        </div>
+const QUERY = `
+query {
+  _getOrdersByTimeInterval {
+    createdAt
+  }
+}
+`;
 
-        <Info />
-      </CardContent>
-    </Card>
-  </div>
-);
+const Dashboard = () => {
+  const [list, setList] = React.useState([]);
+  React.useEffect(() => {
+    const data = fetch(`${process.env.REACT_APP_ACCOUNT_SERVER_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'x-service-security-token-x': localStorage.getItem('service_token'),
+      },
+      body: JSON.stringify({
+        query: QUERY,
+      }),
+    }).then((r) => r.json());
+    data.then((inf) => setList(inf.data?._getOrdersByTimeInterval));
+  }, []);
+  return (
+    <div>
+      <Welcome />
+      <Card>
+        <CardContent className='d-content'>
+          <div className='top'>
+            <Graph />
+            <Settings />
+          </div>
+          <Chart
+            list={list}
+            width={1330}
+            title='Created orders by the last 30 days'
+          />
+          <Info />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default dashboard;
+export default Dashboard;
